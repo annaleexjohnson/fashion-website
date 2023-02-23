6bf32164473d382ad4701090e5f5ba4c71ec0cf8
Annalee X Johnson <87035551+annaleexjohnson@users.noreply.github.com>
2023-02-23 11:55:03 -0600

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './components/Header'
+
+
+function App() {
+  return(
+    <div className="font-sans h-[100vh]">
+      <Header />
+    </div>
+  )
+}
+
+export default App
