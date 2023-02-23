f98b60a5c2947b73e8b74ee1adbefe5648312626
Annalee X Johnson <87035551+annaleexjohnson@users.noreply.github.com>
2023-02-23 14:08:45 -0600

    Add Page2

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import Header from './components/Header'
 import HomePage from './components/HomePage'
+import Page2 from './components/Page2'
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <div className="font-sans h-[100vh] m-h-[100vh] overflow-y-hidden overflow-x-auto flex flex-row">
       <Header />
       <HomePage />
+      <Page2 />
     </div>
   )
 }
