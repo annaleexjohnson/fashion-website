1eedbcfd645941aa265a4638bf2f490a49a93f03
Annalee X Johnson <87035551+annaleexjohnson@users.noreply.github.com>
2023-03-01 14:02:35 -0600

    add props to btn

--- src/assets/MovingBtn.tsx
+++ src/assets/MovingBtn.tsx
@@ -1,19 +1,23 @@
 import IconLogo from "./IconLogo"
 
-interface BtnText {
+interface Props {
     text: string;
+    darkMode: boolean;
 }
 
-const MovingBtn = (props: BtnText)=>{
+const MovingBtn = (props: Props)=>{
     return(
         <div className="flex">
-            <div className="flex w-[30vw] md:w-[8rem] lg:h-[2rem] 2xl:w-[10rem] 2xl:h-[3rem] text-base border rounded-md border-black overflow-x-hidden">
+            <div style={ props.darkMode ? {border: '1px solid white'} : {} } 
+            className="flex w-[30vw] md:w-[8rem] lg:h-[2rem] 2xl:w-[10rem] 2xl:h-[3rem] 
+            text-base border rounded-md border-black overflow-x-hidden">
                 <span className="self-center inline-block w-[100%] whitespace-nowrap
                  animate-[moveTextInfinite_5s_linear_infinite] play hover:pause 2xl:text-lg">
                     {props.text}
                 </span>
             </div>
-            <div className=" flex flex-wrap justify-center content-center w-[2rem] h-[2rem] 2xl:w-[3rem] 2xl:h-[3rem] border rounded-md border-black">
+            <div style={ props.darkMode ? {border: '1px solid white'} : {} }  
+            className=" flex flex-wrap justify-center content-center w-[2rem] h-[2rem] 2xl:w-[3rem] 2xl:h-[3rem] border rounded-md border-black">
                 <IconLogo />
             </div>
         </div>
